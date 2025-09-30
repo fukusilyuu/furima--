10ed58bbe5c8df630000fcfc302248e74fd3b4ac
fukusilyuu <ichigosister@icloud.com>
2025-10-01 05:19:12 +0900

    new.view コントローラー

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,18 @@
 class ItemsController < ApplicationController
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  private
+  def item_params
+    params.require(:item).permit(:name, :image, :price, :explanation, :genre_id, :quality_id, :payment_id, :prefecture_id, :days_id).merge(user_id: current_user.id)
+  end
 end
