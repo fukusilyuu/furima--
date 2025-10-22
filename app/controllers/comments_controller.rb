bb26203af8cb9ab285731f23c568489f39c991d2
fukusilyuu <ichigosister@icloud.com>
2025-10-23 06:52:17 +0900

    コメント edit,destory

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[edit update destroy]
+  before_action :set_item, only: %i[update destroy]
 
   def new
     @comment = Comment.new
@@ -7,10 +8,14 @@ class CommentsController < ApplicationController
 
   def create
     @reply = Reply.new
+
     @comment = Comment.new(comment_params)
-    @item = Item.find(params[:item_id])
+    @comment.user = current_user
+    # @item = Item.find(params[:item_id])
     if @comment.save
+      redirect_to item_path(@comment.item)
       CommentChannel.broadcast_to @item, { comment: @comment, user: @comment.user }
+
     else
       @item = @comment.item
       @comments = @item.comments
@@ -23,10 +28,14 @@ class CommentsController < ApplicationController
   end
 
   def update
-    if @comment.update(comment_params)
-      redirect_to item_path
+    if @comment.user == current_user
+      if @comment.update(comment_params)
+        redirect_to @item, notice: 'コメントを編集しました'
+      else
+        redirect_to @item, alert: 'コメントの編集に失敗しました'
+      end
     else
-      render :edit, status: :unprocessable_entity
+      redirect_to @item, alert: '編集権限がありません'
     end
   end
 
@@ -40,6 +49,10 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   def set_comment
