c37784c34e1fe2ecf7e9f54eb4cfc600986f38ce
fukusilyuu <ichigosister@icloud.com>
2025-10-22 10:18:01 +0900

    返信予定

--- app/javascript/reply.js
+++ app/javascript/reply.js
@@ -1,13 +1,25 @@
 document.addEventListener("turbo:load", () => {
-  const Replies = document.getElementById("replies");
-  if (!Replies) return;
+  const ReplyRan = document.getElementById("reply-ran");
+  if (!ReplyRan) return;
 
-  const Reply = document.getElementById("reply");
-  if (!Reply) return;
+  const ReplyShow = document.getElementById("reply-show");
+  if (!ReplyShow) return;
 
   
-  Replies.addEventListener("click", () => {
+  ReplyRan.addEventListener("click", () => {
     // displayがnoneならblockに、それ以外ならnoneに切り替え
+    if (ReplyShow.style.display === "none" || ReplyShow.classList.contains("hidden")) {
+      ReplyShow.style.display = "block";
+      ReplyShow.classList.remove("hidden");
+    } else {
+      ReplyShow.style.display = "none";
+      ReplyShow.classList.add("hidden");
+    }
+  });
+
+  const Replies = document.getElementById("replies");
+  const Reply = document.getElementById("reply");
+  Replies.addEventListener('click', () => {
     if (Reply.style.display === "none" || Reply.classList.contains("hidden")) {
       Reply.style.display = "block";
       Reply.classList.remove("hidden");
