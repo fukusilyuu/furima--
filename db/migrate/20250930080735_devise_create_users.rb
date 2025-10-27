3df3f4b8e4c01bc8afdc93aae726d3db7ba5287e
fukusilyuu <ichigosister@icloud.com>
2025-10-27 09:26:55 +0900

    icon追加

--- db/migrate/20250930080735_devise_create_users.rb
+++ db/migrate/20250930080735_devise_create_users.rb
@@ -4,9 +4,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :nickname,           null: false, default: ""
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :icon, null: false
+      t.string :nickname,           null: false, default: ''
+      t.string :email,              null: false, default: ''
+      t.string :encrypted_password, null: false, default: ''
       t.string :firstname_kanji, null: false
       t.string :lastname_kanji, null: false
       t.string :firstname_katakana, null: false
@@ -37,7 +38,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
 
       t.timestamps null: false
     end
