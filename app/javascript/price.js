837bb01243701015309ea99bab6bc2144759afc7
fukusilyuu <ichigosister@icloud.com>
2025-11-06 23:57:07 +0900

    条件分岐

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -11,12 +11,24 @@ const price = () => {
 
   const orderprice = document.getElementById('order-price');
   if (!orderprice) return;
-  orderprice.addEventListener('', function(){
-  const salescommission = document.getElementById8('sales-commission');
+
+  const forsellersonly  = document.getElementById('for-sellers-only');
+  if (!forsellersonly) return;
+
+  orderprice.addEventListener('click', function(){
+  
+  if (forsellersonly.style.display === "none" || forsellersonly.classList.contains("hidden")) {
+    forsellersonly.style.display = "block";
+    this.firstChild.classList.remove("hidden");
+    const salescommission = document.getElementById8('sales-commission');
   const salesprofit = document.getElementById('sales-profit');
 
   salescommission.innerHTML = Math.floor(orderprice * 0.1);
   salesprofit.innerHTML = orderprice = salescommission.innerHTML;
+  } else {
+    forsellersonly.style.display = "none";
+    forsellersonly.classList.add("hidden");
+  }
   });
 };
 window.addEventListener("turbo:load", price);
